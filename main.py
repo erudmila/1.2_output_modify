5c6917de50b4960f4268c6205f9d33ef26b27ff3
erudmila <144694341+erudmila@users.noreply.github.com>
2024-03-19 13:05:36 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+#elma rudmila
+#9/14/23
+#the print statement makes the computer output text to a compile
+
+print("I love computer science")
+print("computer science is my first class of the schoool day! \n i have it periods 1 and 2")
+# \n is for creating a new charactr / line 
